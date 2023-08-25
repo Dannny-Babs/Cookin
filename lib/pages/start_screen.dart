171cbb99af2d70a4a61eef7cbd37358f36ffd028
Daniel Babs <dannybabs123456@gmail.com>
2023-08-25 23:13:47 +0100

    gege

--- lib/pages/start_screen.dart
+++ lib/pages/start_screen.dart
@@ -6,6 +6,10 @@ class startScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(),
+      ),
+    );
   }
 }
