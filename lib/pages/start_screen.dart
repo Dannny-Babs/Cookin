cdba6a0470ecfb38dc8229896d412c4d72308342
Daniel Babs <dannybabs123456@gmail.com>
2023-08-25 23:12:57 +0100

    yup

---
+++ lib/pages/start_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class startScreen extends StatelessWidget {
+  const startScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
