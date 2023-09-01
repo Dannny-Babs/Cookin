fc3545b115a87554843c57e370969902bb19c89f
raji <kingraj1344@gmail.com>
2023-09-01 09:43:15 +0100

    done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,14 +4,9 @@ import 'package:cookin/widget/widget.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
